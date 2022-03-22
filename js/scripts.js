c83954cf071e19270d58018764ebc8fe68d3c5cf
Michael Wright <m.wright92@icloud.com>
2022-03-22 16:07:12 -0700

    add hide/show function to each flashcard
    Co-authored-by: Jess Schueler <jessicagrace.schueler@gmail.com>
    Co-authored-by: Meron Tekie  <mtekiesd210309@gmail.com>

---
+++ js/scripts.js
@@ -0,0 +1,56 @@
+$(document).ready(function() {
+  $("#javascript").click(function() {
+    $("#jsdef").toggle();
+    $("#javascript").toggle();
+  });
+  $("#jsdef").click(function() {
+    $("#javascript").toggle();
+    $("#jsdef").toggle();
+  });
+
+  $("#operators").click(function() {
+    $("#opdef").toggle();
+    $("#operators").toggle();
+  });
+  $("#opdef").click(function() {
+    $("#operators").toggle();
+    $("#opdef").toggle();
+  });
+  
+  $("#variables").click(function() {
+    $("#vardef").toggle();
+    $("#variables").toggle();
+  });
+  $("#vardef").click(function() {
+    $("#variables").toggle();
+    $("#vardef").toggle();
+  });
+
+  $("#functions").click(function() {
+    $("#fundef").toggle();
+    $("#functions").toggle();
+  });
+  $("#fundef").click(function() {
+    $("#functions").toggle();
+    $("#fundef").toggle();
+  });
+
+  $("#parameters").click(function() {
+    $("#pardef").toggle();
+    $("#parameters").toggle();
+  });
+  $("#pardef").click(function() {
+    $("#parameters").toggle();
+    $("#pardef").toggle();
+  });
+
+  $("#arguments").click(function() {
+    $("#argdef").toggle();
+    $("#arguments").toggle();
+  });
+  $("#argdef").click(function() {
+    $("#arguments").toggle();
+    $("#argdef").toggle();
+  });
+
+});
